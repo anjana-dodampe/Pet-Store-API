8add511442d15f6f721a2d9c9e8a13705b04ddf8
anjana-dodampe <ahmdodampe@hotmail.com>
2021-11-19 15:30:36 +0530

    setup

--- src/main/java/com/example/petstore/PetResource.java
+++ src/main/java/com/example/petstore/PetResource.java
@@ -1,30 +1,28 @@
 package com.example.petstore;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 @Path("/v1/pets")
 @Produces("application/json")
 public class PetResource {
-
-	@APIResponses(value = {
-			@APIResponse(responseCode = "200", description = "All Pets", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(ref = "Pet"))) })
-	@GET
-	public Response getPets() {
-		List<Pet> pets = new ArrayList<Pet>();
+	
+	List<Pet> pets = new ArrayList<Pet>();
+	
+	public PetResource () {
+		
 		Pet pet1 = new Pet();
 		pet1.setPetId(1);
 		pet1.setPetAge(3);
@@ -46,9 +44,69 @@ public class PetResource {
 		pets.add(pet1);
 		pets.add(pet2);
 		pets.add(pet3);
+	}
+	
+	//Get pet Names
+	
+	@APIResponses(value = {
+			@APIResponse(responseCode = "200", description = "All Pets", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(ref = "Pet"))) })
+	@GET
+	public Response getPets() {
+	
 		return Response.ok(pets).build();
 	}
-
+	
+	
+	// AddPets
+	@APIResponses(value = {
+			@APIResponse(responseCode = "201", description = "Add Pet", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(ref = "Pet"))) })
+	@POST
+	public Response addPet(@RequestBody Pet pet) {
+        pets.add(pet);
+        return Response.created(URI.create("/v1/pets")).build();
+    }
+	
+	//DeletePets
+	@APIResponses(value = {
+			@APIResponse(responseCode = "200", description = "Pet Deleted Successfully!", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(ref = "Pet"))),
+			@APIResponse(responseCode = "404", description = "No Pet found for the id.") })
+	@DELETE
+	@Path("{petId}")
+	public Response deletePet(@PathParam("petId") int petId) {
+		if (petId < 0) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		for (Pet pet : pets) {
+			if(pet.getPetId() == petId) {
+		    	pets.remove(pet);
+			    return Response.noContent().build();
+		    }
+        }
+		return Response.status(Status.NOT_FOUND).build();
+	}
+	
+	//Update
+	@APIResponses(value = {
+			@APIResponse(responseCode = "200", description = "Update Pet", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(ref = "Pet"))),
+			@APIResponse(responseCode = "404", description = "No Pet found for the id") })
+	
+	@PUT
+	@Path("{petId}")
+	public Response updatePet(@PathParam("petId") int petId, @RequestBody Pet newPet) {
+		if (petId < 0) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		for (Pet pet : pets) {
+			if(pet.getPetId() == petId) {
+		    	pets.remove(pet);
+		    	pets.add(newPet);
+			    return Response.ok(newPet).build();
+		    }
+        }
+		return Response.status(Status.NOT_FOUND).build();
+	}
+	
+	//Get Pet Id 
 	@APIResponses(value = {
 			@APIResponse(responseCode = "200", description = "Pet for id", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(ref = "Pet"))),
 			@APIResponse(responseCode = "404", description = "No Pet found for the id.") })
